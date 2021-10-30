3f0c0651877400a13055012afed554f9fa84fbd3
Mark Pokrovsky <solexofficial@yandex.ru>
2021-10-31 00:53:28 +0300

    feat: add React clone element

--- src/app/components/examples/cloneElement/cloneElementExample.jsx
+++ src/app/components/examples/cloneElement/cloneElementExample.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import CardWrapper from "../../common/Card";
+import TextField from "../../common/form/textField";
 
 import SmallTitle from "../../common/typografy/smallTitle";
 const CloneElementExample = () => {
-    return (
-        <CardWrapper>
-            <SmallTitle>Пример</SmallTitle>
-        </CardWrapper>
-    );
+  const field = <TextField label="email" name="email" />;
+  const handleChange = (target) => {
+    console.log("changed", target);
+  };
+  return (
+    <CardWrapper>
+      <SmallTitle>Пример</SmallTitle>
+      {field}
+      {React.cloneElement(field, {
+        onChange: handleChange,
+        label: "Cloned email"
+      })}
+    </CardWrapper>
+  );
 };
 
 export { CloneElementExample };
