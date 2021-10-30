644da2fe1f0a5dd72483d51f7cea2fea47e60950
Mark Pokrovsky <solexofficial@yandex.ru>
2021-10-31 01:15:31 +0300

    feat: react children

--- src/app/components/examples/children/reactChildrenExample.jsx
+++ src/app/components/examples/children/reactChildrenExample.jsx
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CardWrapper from "../../common/Card";
+import Divider from "../../common/divider";
+import TextField from "../../common/form/textField";
 import SmallTitle from "../../common/typografy/smallTitle";
+import PropTypes from "prop-types";
+
+const FormComponent = ({ children }) => {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
+  const handleChange = (target) => {
+    setData((prevState) => ({ ...prevState, [target.name]: target.value }));
+  };
+
+  //   console.log(children);
+  return React.Children.map(children, (child) => {
+    const config = {
+      ...child.props,
+      onChange: handleChange,
+      value: data[child.props.name] || ""
+    };
+
+    return React.cloneElement(child, config);
+  });
+};
+
+FormComponent.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ])
+};
 const ReactChildrenExample = () => {
-    return (
-        <CardWrapper>
-            <SmallTitle>Clone form and add props</SmallTitle>
-        </CardWrapper>
-    );
+  return (
+    <CardWrapper>
+      <SmallTitle>Clone form and add props</SmallTitle>
+      <Divider />
+      <FormComponent>
+        <TextField name="email" label="email" />
+        <TextField name="password" label="Пароль" type="password" />
+      </FormComponent>
+    </CardWrapper>
+  );
 };
 
 export default ReactChildrenExample;
