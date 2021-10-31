bbad399d12030a47b0115fe156467e13d180a698
Mark Pokrovsky <solexofficial@yandex.ru>
2021-10-31 14:27:37 +0300

    feat: OrderedListComponent with react.children.map

--- src/app/components/exercises/childrenExercise.jsx
+++ src/app/components/exercises/childrenExercise.jsx
@@ -1,25 +1,43 @@
 import React from "react";
+import PropTypes from "prop-types";
 import CollapseWrapper from "../common/collapse";
-const ChildrenExercise = () => {
-    return (
-        <CollapseWrapper title="Упражнение">
-            <p className="mt-3">
-                У вас есть компоненты Списка. Вам необходимо к каждому из них
-                добавить порядковый номер, относительно того, как они
-                располагаются на странице. Вы можете использовать как{" "}
-                <code>React.Children.map</code> так и{" "}
-                <code>React.Children.toArray</code>
-            </p>
 
-            <Component />
-            <Component />
-            <Component />
-        </CollapseWrapper>
-    );
+const OrderedListComponent = ({ children }) => {
+  return (
+    <ol>
+      {React.Children.map(children, (child) => {
+        return <li>{child}</li>;
+      })}
+    </ol>
+  );
+};
+
+OrderedListComponent.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ])
+};
+const ChildrenExercise = () => {
+  return (
+    <CollapseWrapper title="Упражнение">
+      <p className="mt-3">
+        У вас есть компоненты Списка. Вам необходимо к каждому из них добавить
+        порядковый номер, относительно того, как они располагаются на странице.
+        Вы можете использовать как <code>React.Children.map</code> так и{" "}
+        <code>React.Children.toArray</code>
+      </p>
+      <OrderedListComponent>
+        <Component />
+        <Component />
+        <Component />
+      </OrderedListComponent>
+    </CollapseWrapper>
+  );
 };
 
 const Component = () => {
-    return <div>Компонент списка</div>;
+  return <div>Компонент списка</div>;
 };
 
 export default ChildrenExercise;
